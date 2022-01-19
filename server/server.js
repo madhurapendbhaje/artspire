217222ed3409528df69b398a38329f04a144cea8
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-19 14:49:48 -0800

    Setup users route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/users");
 
 // Read env variables
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
