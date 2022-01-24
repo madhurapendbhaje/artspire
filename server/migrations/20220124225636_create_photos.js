6292d1c5d18ae7aeebb606aac006df3de3cafa0e
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-24 15:26:42 -0800

    knex migrations for photos table

---
+++ server/migrations/20220124225636_create_photos.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("photos", (table) => {
+        table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+        table.string("url").notNullable();
+        table.string("category").notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable("photos");
+};
