9b66c6b9bdcc965469be39107202d2d036db6bc4
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-24 12:56:47 -0800

    Responsive photo gallery page

--- client/src/pages/InspirePage/InspirePage.js
+++ client/src/pages/InspirePage/InspirePage.js
@@ -10,37 +10,118 @@ class InspirePage extends Component {
     state = {
         photos: [],
         error: false,
+        windowSize: null,
     };
+
+    // https://stackoverflow.com/questions/8188548/splitting-a-js-array-into-n-arrays
+    /**
+     *
+     * @param {array} array     Array to be divided []
+     * @param {integer} parts   No of subset the input array should be divided into Eg: 2
+     * @returns                 [[], []]
+     */
+    splitToChunks(array, parts) {
+        let result = [];
+        const copyArray = [...array];
+        for (let i = parts; i > 0; i--) {
+            result.push(copyArray.splice(0, Math.ceil(copyArray.length / i)));
+        }
+        return result;
+    }
+
+    /**
+     * Function to find the window size
+     */
+    intervalId = null;
+
+    checkWindowSize = () => {
+        this.intervalId = setInterval(() => {
+            if (window.innerWidth !== this.state.windowSize) {
+                this.setState({
+                    windowSize: window.innerWidth,
+                });
+            }
+        }, 100);
+    };
+
     componentDidMount() {
         axios
             .get(
-                `${UNSPLASH_API_URL}/search/photos/?query=sunset&client_id=${UNSPLASH_API_KEY}`
+                `${UNSPLASH_API_URL}/search/photos/?query=sunset&client_id=${UNSPLASH_API_KEY}&per_page=12`
             )
             .then((response) => {
                 this.setState({ photos: response.data.results });
             })
-            .catch((_err) => {
+            .catch((err) => {
+                console.log(err);
                 this.setState({ error: true });
             });
+        this.checkWindowSize();
     }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
     render() {
-        return (
-            <div className="photo-gallery">
-                {this.state.photos.map((photo) => {
-                    return (
-                        <img
-                            src={photo.urls.regular}
-                            alt={
-                                photo.description
-                                    ? photo.description
-                                    : photo.alt_description
-                            }
-                            className="photo-gallery__image"
-                        />
-                    );
-                })}
-            </div>
-        );
+        if (this.state.error) {
+            return <div>Finding photos...</div>;
+        }
+        // For tablet window size, photo gallery will consist of two columns
+        if (this.state.windowSize >= 768 && this.state.windowSize < 1280) {
+            console.log("here");
+            const modifiedPhotoArr = this.splitToChunks(this.state.photos, 2);
+            console.log(modifiedPhotoArr);
+            return (
+                <div className="photo-gallery">
+                    {modifiedPhotoArr.map((photoArr) => {
+                        return (
+                            <div className="photo-gallery__column">
+                                {photoArr.map((photo) => {
+                                    return (
+                                        <img
+                                            src={photo.urls.regular}
+                                            alt={
+                                                photo.description
+                                                    ? photo.description
+                                                    : photo.alt_description
+                                            }
+                                            className="photo-gallery__image"
+                                        />
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
+            );
+        } else {
+            // For desktop window size, photo gallery will consist of three columns
+            const modifiedPhotoArr = this.splitToChunks(this.state.photos, 3);
+            return (
+                <div className="photo-gallery">
+                    {modifiedPhotoArr.map((photoArr) => {
+                        return (
+                            <div className="photo-gallery__column">
+                                {photoArr.map((photo) => {
+                                    return (
+                                        <img
+                                            src={photo.urls.regular}
+                                            alt={
+                                                photo.description
+                                                    ? photo.description
+                                                    : photo.alt_description
+                                            }
+                                            className="photo-gallery__image"
+                                        />
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
+            );
+        }
     }
 }
 
