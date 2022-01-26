43320c078a5fb4ffaf1d057978ca54c161e61967
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-25 23:15:46 -0800

    Nav items with hover effect

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,8 +11,19 @@ function Header() {
                     <img src={logo} alt="Logo" className="header__logo" />
                 </NavLink>
                 <nav className="header__navigation">
-                    <NavLink to="/inspire" className="header__navigation-link">
+                    <NavLink
+                        to="/inspire"
+                        className="header__navigation-link"
+                        activeClassName="header__navigation-link--active"
+                    >
                         Inspire Me
+                    </NavLink>
+                    <NavLink
+                        to="/tutorials"
+                        className="header__navigation-link"
+                        activeClassName="header__navigation-link--active"
+                    >
+                        Tutorials
                     </NavLink>
                 </nav>
             </header>
