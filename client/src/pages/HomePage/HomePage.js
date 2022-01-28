5f25af5cac4dcc0e50a54a1702eae2921d7b4da5
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-27 20:39:13 -0800

    Get started link on home page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import "./HomePage.scss";
 
+import { Link } from "react-router-dom";
+
 import homePageImage from "../../assets/images/home-page.svg";
 
 function HomePage() {
@@ -10,6 +12,9 @@ function HomePage() {
                 alt="home page"
                 className="home-page__image"
             />
+            <Link to="/users/login" className="home-page__link">
+                Get Started
+            </Link>
         </div>
     );
 }
