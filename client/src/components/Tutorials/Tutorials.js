1d154f7b6cf45c120de1b748a03b3dfcf9507a57
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-30 10:07:20 -0800

    Styling updates for tutorials page

--- client/src/components/Tutorials/Tutorials.js
+++ client/src/components/Tutorials/Tutorials.js
@@ -2,7 +2,9 @@ import "./Tutorials.scss";
 
 import { Component } from "react";
 import axios from "axios";
+
 import heartIcon from "../../assets/icons/heart-solid.svg";
+import backIcon from "../../assets/icons/angle-left-solid.svg";
 
 const YOUTUBE_API_URL = process.env.REACT_APP_YOUTUBE_API_URL;
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
@@ -25,33 +27,46 @@ class Tutorials extends Component {
     render() {
         return (
             <div className="video-grid">
-                {this.state.tutorials.map((video) => {
-                    const { id, snippet } = video;
-                    return (
-                        <div
-                            key={id.videoId}
-                            className="video-grid__video-container"
-                        >
-                            <iframe
-                                src={`https://www.youtube.com/embed/${id.videoId}`}
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                className="video-grid__video"
-                            />
-                            <div className="video-grid__caption-container">
-                                <h4 className="video-grid__caption">
-                                    {snippet.title}
-                                </h4>
-                                <img
-                                    src={heartIcon}
-                                    alt="heart icon"
-                                    className="video-grid__icon"
+                <div className="video-grid__save">
+                    <img
+                        src={backIcon}
+                        alt="back icon"
+                        className="color__icon"
+                        onClick={() => {
+                            this.props.history.goBack();
+                        }}
+                    />
+                </div>
+
+                <div className="video-grid__container">
+                    {this.state.tutorials.map((video) => {
+                        const { id, snippet } = video;
+                        return (
+                            <div
+                                key={id.videoId}
+                                className="video-grid__video-frame"
+                            >
+                                <iframe
+                                    src={`https://www.youtube.com/embed/${id.videoId}`}
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                    className="video-grid__video"
                                 />
+                                <div className="video-grid__caption-container">
+                                    <h4 className="video-grid__caption">
+                                        {snippet.title}
+                                    </h4>
+                                    <img
+                                        src={heartIcon}
+                                        alt="heart icon"
+                                        className="video-grid__icon"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
         );
     }
