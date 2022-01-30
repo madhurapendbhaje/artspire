62c766992959764fdd40a8ec99b711c7af86c648
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-29 20:08:02 -0800

    Update photos post api to relate photo with user id

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -5,7 +5,6 @@ const { v4: uuidv4 } = require("uuid");
 const knex = require("../knexConfig");
 
 router.post("/", (req, res) => {
-    console.log(req.body);
     const photoObj = {
         id: uuidv4(),
         url: req.body.url,
@@ -14,6 +13,16 @@ router.post("/", (req, res) => {
     knex("photos")
         .insert(photoObj)
         .then(() => {
+            const userPhotoObj = {
+                id: uuidv4(),
+                user_id: req.body.user_id,
+                photo_id: photoObj.id,
+            };
+            knex("user_photo")
+                .insert(userPhotoObj)
+                .catch((_err) =>
+                    res.status(500).send("Could not save to favorites")
+                );
             return res.status(201).send("Saved to photos");
         })
         .catch((_err) => res.status(400).send("Could not save the photo"));
