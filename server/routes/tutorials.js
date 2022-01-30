511aab626cbea78000212ec9c996d8d32aa9abba
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-30 10:33:42 -0800

    API post

--- server/routes/tutorials.js
+++ server/routes/tutorials.js
@@ -4,27 +4,27 @@ const { v4: uuidv4 } = require("uuid");
 const knex = require("../knexConfig");
 
 router.post("/", (req, res) => {
-    const photoObj = {
+    const tutorialObj = {
         id: uuidv4(),
         url: req.body.url,
         category: req.body.category,
     };
-    knex("photos")
-        .insert(photoObj)
+    knex("tutorials")
+        .insert(tutorialObj)
         .then(() => {
-            const userPhotoObj = {
+            const userTutorialObj = {
                 id: uuidv4(),
                 user_id: req.body.user_id,
-                photo_id: photoObj.id,
+                tutorial_id: photoObj.id,
             };
-            knex("user_photo")
-                .insert(userPhotoObj)
+            knex("user_tutorial")
+                .insert(userTutorialObj)
                 .catch((_err) =>
                     res.status(500).send("Could not save to favorites")
                 );
-            return res.status(201).send("Saved to photos");
+            return res.status(201).send("Saved to tutorials");
         })
-        .catch((_err) => res.status(400).send("Could not save the photo"));
+        .catch((_err) => res.status(400).send("Could not save the tutorial"));
 });
 
 module.exports = router;
