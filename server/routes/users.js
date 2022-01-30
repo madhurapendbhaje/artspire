e61ade710f9d74317bd97568007b099158b33ae9
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-29 18:30:26 -0800

    PUT API to update user profile preferences

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const { v4: uuidv4 } = require("uuid");
+
+const knex = require("../knexConfig");
 
 // create a login failure endpoint
 router.get("/loginFailed", (_req, res) => {
@@ -37,4 +40,56 @@ router.get("/check-auth", (req, res) => {
     res.status(200).json(req.user);
 });
 
+// Update user profile
+router.put("/:id", (req, res) => {
+    const data = req.body;
+
+    knex("users")
+        .where({ id: req.params.id })
+        .update({ proficiency_level: data.level })
+        .catch((_err) => {
+            return res.status(500).send("Could not save preferences");
+        });
+
+    data.medium.forEach((medium) => {
+        knex("medium")
+            .where({ medium_type: medium })
+            .then((response) => {
+                const mediumId = response[0].id;
+                const userMediumObj = {
+                    id: uuidv4(),
+                    user_id: req.params.id,
+                    medium_id: mediumId,
+                };
+                knex("user_medium")
+                    .insert(userMediumObj)
+                    .then(() => {
+                        knex("users")
+                            .where({ id: req.params.id })
+                            .update({ is_profile_complete: true })
+                            .catch((_err) => {
+                                return res
+                                    .status(500)
+                                    .send(
+                                        "Could not save profile preferences."
+                                    );
+                            });
+                    })
+                    .catch((_err) => {
+                        return res
+                            .status(500)
+                            .send("Could not save preferences");
+                    });
+            })
+            .catch((_err) => {
+                return res.status(500).send("Could not save preferences");
+            });
+    });
+
+    res.send("User preferences saved");
+});
+
+router.get("/", (req, res) => {
+    res.send("Test");
+});
 module.exports = router;
