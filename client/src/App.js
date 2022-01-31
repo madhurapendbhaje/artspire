673c0ceb39d19462847fdb573491e82421fc2acf
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-30 23:30:45 -0800

    Tutorials Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import AuthButton from "./components/AuthButton";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-import UserProfile from "./components/UserProfile";
 import Protected from "./components/Protected";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
@@ -12,6 +10,7 @@ import ProfilePage from "./pages/ProfilePage";
 import InspirePage from "./pages/InspirePage";
 import ColorsPage from "./pages/ColorsPage";
 import Tutorials from "./components/Tutorials";
+import TutorialsPage from "./pages/TutorialsPage";
 
 class App extends Component {
     render() {
@@ -26,26 +25,16 @@ class App extends Component {
                     />
                     <Protected exact path="/inspire" component={InspirePage} />
                     <Protected
+                        exact
+                        path="/tutorials"
+                        component={TutorialsPage}
+                    />
+                    <Protected
                         path="/tutorials/:category"
                         component={Tutorials}
                     />
                     <Route path="/users/login" component={LoginPage} />
                     <Protected path="/users/profile" component={ProfilePage} />
-                    {/* <AuthButton />
-                    <ul>
-                        <li>
-                            <Link to="/users">Home Page</Link>
-                        </li>
-                        <li>
-                            <Link to="/users/favorites">Protected Page</Link>
-                        </li>
-                    </ul>
-                    <Route path="/users/login" component={LoginPage} />
-                    <Route path="/users" component={HomePage} />
-                    <Favorites
-                        path="/users/favorites"
-                        component={UserProfile}
-                    /> */}
                 </div>
             </Router>
         );
---
+++ client/src/pages/TutorialsPage/TutorialsPage.js
@@ -0,0 +1,13 @@
+import "./TutorialsPage.scss";
+
+import { Component } from "react";
+import axios from "axios";
+
+class TutorialsPage extends Component {
+    componentDidMount() {}
+    render() {
+        return <div></div>;
+    }
+}
+
+export default TutorialsPage;
